ae54082a39837bfd1dd7773e6cc035bf2c270ffb
takuro913 <atkgdsfhsk.jp@gmail.com>
2023-09-26 23:16:06 +0900

    資料7.10の途中でマッスルメモが表示されず終了

--- app/Http/Controllers/TrainingController.php
+++ app/Http/Controllers/TrainingController.php
@@ -14,7 +14,8 @@ class TrainingController extends Controller
      */
     public function index()
     {
-        return response()->view('training.index');
+       $training = Training::getAllOrderByUpdated_at();
+       return response()->view('training.index',compact('training'));
     }
 
     /**
@@ -30,15 +31,32 @@ class TrainingController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      // バリデーション
+      $validator = Validator::make($request->all(), [
+        'training' => 'required | max:191',
+        'comment' => 'required',
+      ]);
+        // バリデーション:エラー
+      if ($validator->fails()) {
+        return redirect()
+          ->route('training.create')
+          ->withInput()
+          ->withErrors($validator);
+      }
+        // create()は最初から用意されている関数
+        // 戻り値は挿入されたレコードの情報
+      $result = Training::create($request->all());
+        // ルーティング「todo.index」にリクエスト送信（一覧ページに移動）
+      return redirect()->route('training.index');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        $training = Training::find($id);
+        return response()->view('training.show', compact('training'));
     }
 
     /**
